737681f1f70bac3e157265b9e0ece9eb0d774412
Maksym Kundeev <mkundeev@gmail.com>
2022-07-06 14:58:54 +0300

    change to hooks pokemon and modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,39 +1,66 @@
 import { createPortal } from "react-dom";
-import { Component } from "react";
+import { useEffect } from "react";
 
 import s from "./Modal.module.css";
 
 const portalRef = document.querySelector("#modal-root");
 
-class Modal extends Component {
-  onEscPressed = (event) => {
-    if (event.code === "Escape") {
-      this.props.onClose();
-    }
-  };
-
-  onBackdropClick = (event) => {
+export default function Modal({ onClose, children }) {
+  const onBackdropClick = (event) => {
     if (event.target === event.currentTarget) {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  componentDidMount() {
-    document.addEventListener("keydown", this.onEscPressed);
-  }
+  useEffect(() => {
+    const onEscPressed = (event) => {
+      if (event.code === "Escape") {
+        onClose();
+      }
+    };
+    document.addEventListener("keydown", onEscPressed);
+    return () => document.removeEventListener("keydown", onEscPressed);
+  }, [onClose]);
 
-  componentWillUnmount() {
-    document.removeEventListener("keydown", this.onEscPressed);
-  }
-
-  render() {
-    return createPortal(
-      <div className={s.backdrop} onClick={this.onBackdropClick}>
-        <div className={s.modal}>{this.props.children}</div>
-      </div>,
-      portalRef
-    );
-  }
+  return createPortal(
+    <div className={s.backdrop} onClick={onBackdropClick}>
+      <div className={s.modal}>{children}</div>
+    </div>,
+    portalRef
+  );
 }
 
-export default Modal;
+// const portalRef = document.querySelector("#modal-root");
+
+// class Modal extends Component {
+//   onEscPressed = (event) => {
+//     if (event.code === "Escape") {
+//       this.props.onClose();
+//     }
+//   };
+
+//   onBackdropClick = (event) => {
+//     if (event.target === event.currentTarget) {
+//       this.props.onClose();
+//     }
+//   };
+
+//   componentDidMount() {
+//     document.addEventListener("keydown", this.onEscPressed);
+//   }
+
+//   componentWillUnmount() {
+//     document.removeEventListener("keydown", this.onEscPressed);
+//   }
+
+//   render() {
+//     return createPortal(
+//       <div className={s.backdrop} onClick={this.onBackdropClick}>
+//         <div className={s.modal}>{this.props.children}</div>
+//       </div>,
+//       portalRef
+//     );
+//   }
+// }
+
+// export default Modal;
--- src/components/Pokemon/Pokemon.js
+++ src/components/Pokemon/Pokemon.js
@@ -1,46 +1,91 @@
-import { Component } from "react";
+import { useState, useEffect } from "react";
 import Modal from "../Modal/Modal";
 
-class Pokemon extends Component {
-  state = {
-    pokemons: [],
-    photo: "",
-  };
+export default function Pokemon() {
+  const [pokemons, setPokemons] = useState([]);
+  const [photo, setPhoto] = useState("");
+  const [showModal, setShowModal] = useState(false);
 
-  componentDidMount() {
-    fetch("https://pokeapi.co/api/v2/pokemon")
-      .then((res) => res.json())
-      .then((res) => this.setState({ pokemons: res.results }));
-  }
-  handleClick = ({ currentTarget: { id } }) => {
+  const handleClick = ({ currentTarget: { id } }) => {
     console.log(id);
     fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
       .then((res) => res.json())
-      .then((res) => this.setState({ photo: res.sprites.front_default }));
+      .then((res) => {
+        setPhoto(res.sprites.front_default);
+        toggleModal();
+      });
   };
-    
 
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon")
+      .then((res) => res.json())
+      .then((res) => setPokemons(res.results));
+  }, []);
 
-  render() {
-    console.log(this.state.photo);
-    const { pokemons, photo } = this.state;
-    return (
-      <div>
-        <ul>
-          {pokemons.map(({ name, url }) => (
-            <li key={name} id={name} onClick={this.handleClick}>
-              <h2>{name}</h2>
-              {/* <img src={this.state.photo} alt={name} width={40} /> */}
-            </li>
-          ))}
-        </ul>
-        {photo ? (
-          <Modal>
-            <img src={photo} alt="alt" width="140"/>
-          </Modal>
-        ) : null}
-      </div>
-    );
-  }
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  };
+
+  // console.log(this.state.photo);
+  //   const { pokemons, photo } = this.state;
+
+  return (
+    <div>
+      <ul>
+        {pokemons.map(({ name }) => (
+          <li key={name} id={name} onClick={handleClick}>
+            <h2>{name}</h2>
+            {/* <img src={this.state.photo} alt={name} width={40} /> */}
+          </li>
+        ))}
+      </ul>
+      {showModal ? (
+        <Modal onClose={toggleModal}>
+          <img src={photo} alt="alt" width="140" />
+        </Modal>
+      ) : null}
+    </div>
+  );
 }
-export default Pokemon;
+
+// class Pokemon extends Component {
+//   state = {
+//     pokemons: [],
+//     photo: "",
+//   };
+
+//   componentDidMount() {
+//     fetch("https://pokeapi.co/api/v2/pokemon")
+//       .then((res) => res.json())
+//       .then((res) => this.setState({ pokemons: res.results }));
+//   }
+//   handleClick = ({ currentTarget: { id } }) => {
+//     console.log(id);
+//     fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+//       .then((res) => res.json())
+//       .then((res) => this.setState({ photo: res.sprites.front_default }));
+//   };
+
+//   render() {
+//     console.log(this.state.photo);
+//     const { pokemons, photo } = this.state;
+//     return (
+//       <div>
+//         <ul>
+//           {pokemons.map(({ name, url }) => (
+//             <li key={name} id={name} onClick={this.handleClick}>
+//               <h2>{name}</h2>
+//               {/* <img src={this.state.photo} alt={name} width={40} /> */}
+//             </li>
+//           ))}
+//         </ul>
+//         {photo ? (
+//           <Modal>
+//             <img src={photo} alt="alt" width="140"/>
+//           </Modal>
+//         ) : null}
+//       </div>
+//     );
+//   }
+// }
+// export default Pokemon;
